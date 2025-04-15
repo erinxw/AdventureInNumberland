726e2d1181b4525a208854409311c63937774def
erinxw <erinkxw@gmail.com>
2025-04-15 17:14:55 +0800

    update drag and snapping apple into basket

---
+++ Assets/ItemSnapToBasket.cs
@@ -0,0 +1,44 @@
+using UnityEngine;
+
+public class ItemSnapToBasket : MonoBehaviour
+{
+    public GameObject snapPointObject; // Drag it in the Inspector
+
+    private static int totalSnapped = 0;
+    private static int totalItems;
+
+    void Start()
+    {
+        if (totalItems == 0)
+        {
+            totalItems = GameObject.FindGameObjectsWithTag("FoodItem").Length;
+            Debug.Log("Total food items: " + totalItems);
+        }
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Basket"))
+        {
+            Debug.Log("Item collected: " + gameObject.name);
+            gameObject.SetActive(false); // Hide the item
+
+            if (snapPointObject != null)
+            {
+                Debug.Log("SnapPoint reference exists: " + snapPointObject.name);
+                snapPointObject.SetActive(true);
+            }
+            else
+            {
+                Debug.LogWarning("SnapPoint not assigned in Inspector!");
+            }
+
+            totalSnapped++;
+
+            if (totalSnapped == totalItems)
+            {
+                Debug.Log("All items have been placed into the basket!");
+            }
+        }
+    }
+}

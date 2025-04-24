462adb303071ec8d9dd7ee9aa711ffddfe379fb1
erinxw <erinkxw@gmail.com>
2025-04-25 00:43:01 +0800

    25/4

--- Assets/7 Scripts/Add Module/BasketManager.cs
+++ Assets/7 Scripts/Add Module/BasketManager.cs
@@ -1,19 +1,42 @@
 using UnityEngine;
+using System.Collections;
 
 public class BasketManager : MonoBehaviour
 {
-    public Animator animator; // Drag your object with the Animator here in the Inspector
-    public int totalItems; // Total number of items to collect
+    public Animator animator;
+    public int totalItems;
     private int itemsCollected = 0;
+
+    public AudioSource[] audioSources; // Set up in Inspector: "One", "Two", etc.
+    private int currentAudioIndex = 0;
+    private bool isPlayingAudio = false;
 
     public void ItemCollected()
     {
         itemsCollected++;
         Debug.Log("Item collected! Total: " + itemsCollected);
 
-        if (itemsCollected >= totalItems)
+        if (currentAudioIndex < audioSources.Length)
         {
-            Debug.Log("All items collected! Playing animation.");
+            StartCoroutine(PlayNextAudio());
+        }
+
+        // Animation will be triggered from the coroutine when audio finishes
+    }
+
+    IEnumerator PlayNextAudio()
+    {
+        isPlayingAudio = true;
+
+        audioSources[currentAudioIndex].Play();
+        yield return new WaitForSeconds(audioSources[currentAudioIndex].clip.length);
+        currentAudioIndex++;
+
+        isPlayingAudio = false;
+
+        if (itemsCollected >= totalItems && currentAudioIndex >= audioSources.Length)
+        {
+            Debug.Log("All items collected & audio done! Playing animation.");
             animator.SetTrigger("ShowChoices");
         }
     }
